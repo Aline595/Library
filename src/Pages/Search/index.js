2afdd99f6a2b8e55a9d51cda1f4ef324c300ef3d
Aline Soares da Silva <aline.as385@gmail.com>
2021-02-06 18:00:11 -0300

    Adicionando GitCorner

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,13 +1,14 @@
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
+import GitHubCorner from '../../components/GitHubCorner';
 
 function App() {
   return (
     <>
       <Header/>
+      <GitHubCorner projectUrl="https://github.com/Aline595/Library"/>
       <Input />
-      <Footer />
     </>
   );
 }
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,71 +2,73 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 import './styles.css';
+import Footer from '../Footer';
 
 function Input(){
 
-    const [book, setBook] = useState("");
-    const [result, setResult] = useState([]);
-    const apiKey = process.env.REACT_APP_API_KEY_GOOGLE;
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const apiKey = process.env.REACT_APP_API_KEY_GOOGLE;
+
+  function handleChange(e){
+    const book = e.target.value;
+    setBook(book);
+  }
   
-    function handleChange(e){
-      const book = e.target.value;
-      setBook(book);
-    }
-    
-    function handleSubmit(e){
-      e.preventDefault();
-      //console.log(book);
-      axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey)
-      .then(data => {
-        //console.log(data);
-        //console.log(data.data.items);
-        setResult(data.data.items);
-      });
-    }
+  function handleSubmit(e){
+    e.preventDefault();
+    //console.log(book);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey)
+    .then(data => {
+      //console.log(data);
+      //console.log(data.data.items);
+      setResult(data.data.items);
+    });
+  }
 
-    return (
-        <div className="container">
-        <div className="search-container">
-          <form onSubmit={handleSubmit} className="form-inline">
-            <div class="form">
-              <input 
-                id="teste"
-                type="text" 
-                onChange={handleChange}
-                className="form-control mt-10" 
-                placeholder="Digite um tema, autor ou título de livro para pesquisar" 
-                autoComplete="off"
-                />
-              <button type="submit" className="btn ">Pesquisar</button>
-            </div>
-          </form>
-        </div>
-
-        <div className=" grid-container">  
-          {result.map(book => (
-            <div className="grid-item">
-              <div className="image">
-                <a target="_blank" key={book.id} href={book.volumeInfo.previewLink}>
-                  <img  src={book.volumeInfo.imageLinks === undefined ? "" : book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
-                </a>
-              </div>
-              <div className="infos">
-                <p className="name_book"><b>Nome do livro: </b>{book.volumeInfo.title}</p>
-                <p className="name_author"><b>Nome do autor: </b>{book.volumeInfo.authors}</p>
-                <p className="description"><b>Editora: </b>{book.volumeInfo.publisher}</p>
-                <p className="ano"><b>Data de Publicação: </b>{book.volumeInfo.publishedDate}</p>
-              </div>
-                <div className="button-container">
-                  <a target="_blank" key={book.id} href={book.volumeInfo.previewLink}>
-                    <button id="description-button">Ler amostra ou comprar</button>
-                  </a>
-                </div>
-            </div>
-          ))}
+  return (
+    <div className="container">
+      <div className="search-container">
+        <form onSubmit={handleSubmit} className="form-inline">
+          <div class="form">
+            <input 
+              id="teste"
+              type="text" 
+              onChange={handleChange}
+              className="form-control mt-10" 
+              placeholder="Digite um tema, autor ou título de livro para pesquisar" 
+              autoComplete="off"
+            />
+            <button type="submit" className="btn ">Pesquisar</button>
           </div>
+        </form>
       </div>
-    );
+
+      <div className=" grid-container">  
+        {result.map(book => (
+          <div className="grid-item">
+            <div className="image">
+              <a target="_blank" key={book.id} href={book.volumeInfo.previewLink}>
+                <img  src={book.volumeInfo.imageLinks === undefined ? "" : book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+              </a>
+            </div>
+            <div className="infos">
+              <p className="name_book"><b>Nome do livro: </b>{book.volumeInfo.title}</p>
+              <p className="name_author"><b>Nome do autor: </b>{book.volumeInfo.authors}</p>
+              <p className="description"><b>Editora: </b>{book.volumeInfo.publisher}</p>
+              <p className="ano"><b>Data de Publicação: </b>{book.volumeInfo.publishedDate}</p>
+            </div>
+            <div className="button-container">
+              <a target="_blank" key={book.id} href={book.volumeInfo.previewLink}>
+                <button id="description-button" className="btn">Ler amostra ou comprar</button>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer/>
+    </div>
+  );
 }
 
 export default Input;
